b65eb58a4d231727bafbb02ca5fd61ad45d36ae2
孙程 <277469661@qq.com>
2016-05-25 14:13:24 +0800

    init
    init

---
+++ SCNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCNetworking
+//
+//  Created by 孙程 on 16/5/25.
+//  Copyright © 2016年 Suncheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
